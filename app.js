755f8fce051da97b9c1c34f12a9c4cab7e8d9804
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-09-18 04:29:44 +0700

    Create docker image

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const app = express()
 const cors = require('cors')
 const index = require('./routes')
 const errorHandler =  require('./middlewares/errorHandler')
+const os = require('os')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send('Hello World, Server Kang Cukur Active, with os: ' + os.type())
+})
 app.use("/", index)
 app.use(errorHandler)
 
