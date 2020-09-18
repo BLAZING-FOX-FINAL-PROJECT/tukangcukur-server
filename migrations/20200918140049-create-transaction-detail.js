0dade3458b6e5ca11c8a6236188a830787ada235
arissatya <arissatya.nugraha@gmail.com>
2020-09-18 21:01:44 +0700

    setup conjungtion table

---
+++ migrations/20200918140049-create-transaction-detail.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TransactionDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      TransactionId: {
+        type: Sequelize.INTEGER
+      },
+      VarianId: {
+        type: Sequelize.INTEGER
+      },
+      Jumlah: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TransactionDetails');
+  }
+};
